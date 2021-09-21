71885f645a75f001eab284e168a786a9c85dc8c8
Nseabasi Udondian <nseabasiudondian@gmail.com>
2021-09-21 17:05:37 +0100

    learnin tailwind in udemy

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    colors:{
+      green: '#19cb98',
+      purple: '#754ffe',
+      fontal: '#f5f4f8',
+      white: '#ffffff',
+      lightPurple: '#e8deff',
+      footercolor: '#edebf1',
+      gray: '#c4c4c4'
+    },
+    fontFamily:{
+      'ham' : [
+        'Harmattan', 'sans-serif'
+      ]
+    },
+    container: {
+      center: true,
+      padding: '4.5rem'      
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
